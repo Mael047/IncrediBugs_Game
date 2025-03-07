7f8c73b0ff150ed634ee74ae3c250988451ab327
Mael047 <joanmanuel.jmmp@gmail.com>
2025-03-07 11:11:28 -0500

    Botón de Guardar

--- Assets/CharacterCustom.cs
+++ Assets/CharacterCustom.cs
@@ -5,18 +5,36 @@ public class CharacterCustom : MonoBehaviour
 {
 
     [Header("Sprites a cambiar")]
-    public SpriteRenderer headParts;
+    public SpriteRenderer HeadParts;
 
     [Header("Sprites ciclicos")]
-    public List<Sprite> headSprites = new List<Sprite>();
-    void Start()
+    public List<Sprite> Options = new List<Sprite>();
+
+    private int CurrentOption = 0;
+
+
+    public void NextOption()
     {
-        
+        CurrentOption++;
+        if (CurrentOption >= Options.Count)
+        {
+            CurrentOption = 0;
+        }
+
+        HeadParts.sprite = Options[CurrentOption];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PreviousOption()
     {
-        
+        CurrentOption--;
+        if (CurrentOption <= 0)
+        {
+            CurrentOption = Options.Count - 1;
+        }
+
+        HeadParts.sprite = Options[CurrentOption];
     }
+
+
 }
+
