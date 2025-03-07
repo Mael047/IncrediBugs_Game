f55c6b18b4446272749388cb4cc513a84de9deb8
Mael047 <joanmanuel.jmmp@gmail.com>
2025-03-07 09:21:51 -0500

    Options

---
+++ Assets/CharacterCustom.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCustom : MonoBehaviour
+{
+
+    [Header("Sprites a cambiar")]
+    public SpriteRenderer headParts;
+
+    [Header("Sprites ciclicos")]
+    public List<Sprite> headSprites = new List<Sprite>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
