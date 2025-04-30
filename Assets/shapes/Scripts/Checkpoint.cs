18f6d935fd4fc4b83aaacd3d6d49c55270b13ce9
leunameek <leunameek@gmail.com>
2025-04-30 18:29:56 -0500

    doneeeee

--- Assets/shapes/Scripts/Checkpoint.cs
+++ Assets/shapes/Scripts/Checkpoint.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Checkpoint : MonoBehaviour, IPointerEnterHandler
-{
-    public int checkpointIndex; // Set this in Inspector
+public class Checkpoint : MonoBehaviour, IPointerEnterHandler {
+    public int checkpointIndex;
     public TracingManager tracingManager;
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
+    public void OnPointerEnter(PointerEventData eventData) {
         tracingManager.CheckpointHit(checkpointIndex);
     }
 }
--- Assets/shapes/Scripts/DrawTrail.cs
+++ Assets/shapes/Scripts/DrawTrail.cs
@@ -1,34 +1,28 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class DrawTrail : MonoBehaviour
-{
+public class DrawTrail : MonoBehaviour {
     public LineRenderer linePrefab;
     private LineRenderer currentLine;
     private List<Vector2> points = new List<Vector2>();
     private List<LineRenderer> activeLines = new List<LineRenderer>();
 
 
-    void Update()
-    {
+    void Update() {
 #if UNITY_EDITOR || UNITY_STANDALONE
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButtonDown(0)) {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             StartLine(mousePos);
         }
-        else if (Input.GetMouseButton(0))
-        {
+        else if (Input.GetMouseButton(0)) {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             UpdateLine(mousePos);
         }
-        else if (Input.GetMouseButtonUp(0))
-        {
+        else if (Input.GetMouseButtonUp(0)) {
             EndLine();
         }
 #else
-        if (Input.touchCount > 0)
-        {
+        if (Input.touchCount > 0) {
             Touch touch = Input.GetTouch(0);
             Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
 
@@ -42,30 +36,25 @@ public class DrawTrail : MonoBehaviour
 #endif
     }
 
-    void StartLine(Vector2 position)
-    {
+    void StartLine(Vector2 position) {
         currentLine = Instantiate(linePrefab);
-        activeLines.Add(currentLine); // Track line
+        activeLines.Add(currentLine);
         points.Clear();
         points.Add(position);
         currentLine.positionCount = 1;
         currentLine.SetPosition(0, new Vector3(position.x, position.y, -0.1f));
     }
 
-    void UpdateLine(Vector2 position)
-    {
-        if (Vector2.Distance(points[points.Count - 1], position) > 0.05f)
-        {
+    void UpdateLine(Vector2 position) {
+        if (Vector2.Distance(points[points.Count - 1], position) > 0.05f) {
             points.Add(position);
             currentLine.positionCount++;
             currentLine.SetPosition(currentLine.positionCount - 1, new Vector3(position.x, position.y, 0));
         }
     }
 
-    void EndLine()
-    {
-        Debug.Log("🔴 End line");
-        // Optionally: Add logic here to check if trace overlaps checkpoints, etc.
+    void EndLine() {
+        Debug.Log("Linea final");
     }
 
     public void ClearLines() {
