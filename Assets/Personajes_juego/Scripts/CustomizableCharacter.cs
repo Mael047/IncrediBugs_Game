d08b00bf46b583d1ed7fbc88c5ed47c2b5bf34dd
Mael047 <joanmanuel.jmmp@gmail.com>
2025-05-11 19:00:33 -0500

    mejoras

--- Assets/Personajes_juego/Scripts/CustomizableCharacter.cs
+++ Assets/Personajes_juego/Scripts/CustomizableCharacter.cs
@@ -7,15 +7,22 @@ using UnityEngine.SceneManagement;
 public class CustomizableCharacter : MonoBehaviour
 {
     [SerializeField]
-    private CustomizedCharacter _character;
+    private CustomizedCharacter _characterTemplate; // Solo para referencia
+
+    private static List<CustomizationData> _customizationDataCache = new(); // Runtime cache
+
+    public static List<CustomizationData> CachedData => _customizationDataCache;
+
     public void StoreCustomizationInformation()
     {
         var elements = GetComponentsInChildren<CustomizableElement>();
-        _character.Data.Clear();
+        _customizationDataCache.Clear();
         foreach (var element in elements)
         {
-            _character.Data.Add(element.GetCustomizationData());
+            _customizationDataCache.Add(element.GetCustomizationData());
         }
+
+        // Cambia de escena
         TransicionEscenasUI.Instance.BloqueSalida(1);
     }
 }
--- Assets/Personajes_juego/Scripts/CustomizedCharacterElement.cs
+++ Assets/Personajes_juego/Scripts/CustomizedCharacterElement.cs
@@ -4,24 +4,20 @@ using System.Linq;
 
 public class CustomizedCharacterElement : MonoBehaviour
 {
-
     [field: SerializeField]
     public CustomizationType Type { get; private set; }
 
-    [field: SerializeField]
-    private CustomizedCharacter _character;
-
     private SpriteRenderer _spriteRenderer;
-
 
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        var customization = _character.Data.FirstOrDefault(d => d.Type == Type );
+
+        var customization = CustomizableCharacter.CachedData.FirstOrDefault(d => d.Type == Type);
         if (customization == null) return;
+
         _spriteRenderer.color = customization.Color;
         _spriteRenderer.sprite = customization.Sprite.Sprite;
         transform.localPosition = customization.Sprite.PositionModifier;
     }
-
 }
