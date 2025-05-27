c1d18f43c2755dd80e2fbc62666a48b0b3a57076
Mael047 <joanmanuel.jmmp@gmail.com>
2025-05-27 14:21:36 -0500

    Nivel sumas y restas

--- Assets/Mecanicas/Sumas_restas/Sumas_restas.cs
+++ Assets/Mecanicas/Sumas_restas/Sumas_restas.cs
@@ -8,8 +8,27 @@ public class Sumas_restas : MonoBehaviour
     [SerializeField] private TMP_Text numberText;
     [SerializeField] private TMP_Text numberText2;
     [SerializeField] private Button[] answerButtons;
+    [SerializeField] private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    [SerializeField] private AudioClip sonidoAcabar;
 
     private int correctAnswer;
+
+    public Animator animator;
+    [Header("Caracter")]
+    public GameObject Caracter;
+
+    [Header("Indicar la seccion del nivel")] // Ingles - Matematica - Geografia - Musica
+    public string SeccionNivel;
+
+    private int puntos = 0;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
 
     void Start()
     {
@@ -26,7 +45,7 @@ public class Sumas_restas : MonoBehaviour
         if (isAddition)
         {
             numberText.text = randomNumber1.ToString();
-            numberText2.text = " + " + randomNumber2.ToString();
+            numberText2.text = " + " + randomNumber2.ToString() + " = ? ";
             correctAnswer = randomNumber1 + randomNumber2;
         }
         else
@@ -36,7 +55,7 @@ public class Sumas_restas : MonoBehaviour
             int min = Mathf.Min(randomNumber1, randomNumber2);
 
             numberText.text = max.ToString();
-            numberText2.text = " - " + min.ToString();
+            numberText2.text = " - " + min.ToString() + " = ? " ;
             correctAnswer = max - min;
         }
 
@@ -69,6 +88,8 @@ public class Sumas_restas : MonoBehaviour
                 answerButtons[i].onClick.AddListener(() =>
                 {
                     Debug.Log("Correcto");
+                    puntos++;
+                    Debug.Log("Puntos: " + puntos);
                     checkAnswer(true);
                 });
             }
@@ -87,8 +108,17 @@ public class Sumas_restas : MonoBehaviour
     {
         if (isCorrect)
         {
-            Debug.Log("Correcto");
-            GenerateNumbers();
+            if (puntos >= 5)
+            {   
+                Debug.Log("Has ganado el juego");
+                canvasGroup.blocksRaycasts = false;
+                StartCoroutine(Second());
+            }
+            else if(puntos < 5)
+            {
+                Debug.Log("Correcto");
+                GenerateNumbers();
+            }
         }
         else
         {
@@ -100,5 +130,41 @@ public class Sumas_restas : MonoBehaviour
     {
         yield return null;
         GenerateNumbers();
+    }
+
+    IEnumerator Second()
+    {
+        animator.SetFloat("Felicitacion", 1);
+        Debug.Log("Animacion completada");
+        yield return new WaitForSeconds(3f);
+        ControladorSonido.Instance.EjecutarSonido(sonidoAcabar);
+        canvasGroup.alpha = 0.8f;
+        yield return new WaitForSeconds(0.1f);
+        canvasGroup.alpha = 0.5f;
+        yield return new WaitForSeconds(0.1f);
+        canvasGroup.alpha = 0.3f;
+        yield return new WaitForSeconds(0.1f);
+        canvasGroup.alpha = 0.1f;
+        yield return new WaitForSeconds(0.1f);
+        canvasGroup.alpha = 0f;
+        yield return new WaitForSeconds(5f);
+        Debug.Log("Cambiar Canvas" + SeccionNivel);
+        Debug.Log("Cargando nivel");
+        if (SeccionNivel == "Ingles")
+        {
+            TransicionEscenasUI.Instance.BloqueSalida("Menu_Ingles");
+        }
+        else if (SeccionNivel == "Matematica")
+        {
+            TransicionEscenasUI.Instance.BloqueSalida("Menu_Math");
+        }
+        else if (SeccionNivel == "Geografia")
+        {
+            TransicionEscenasUI.Instance.BloqueSalida("Menu_Geografia");
+        }
+        else if (SeccionNivel == "Musica")
+        {
+            TransicionEscenasUI.Instance.BloqueSalida("Menu_Musica");
+        }
     }
 }
