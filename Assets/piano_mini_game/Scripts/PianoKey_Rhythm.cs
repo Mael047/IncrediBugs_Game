8aef4d9dd411f3a7ebd97a34cfc8c05ca0b21d7e
leunameek <leunameek@gmail.com>
2025-05-28 21:58:22 -0500

    more and more bug-fixes xD

--- Assets/piano_mini_game/Scripts/PianoKey_Rhythm.cs
+++ Assets/piano_mini_game/Scripts/PianoKey_Rhythm.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
 
 public class PianoKey_Rhythm : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
     public string noteName;
     public AudioClip noteSound;
 
     private AudioSource audioSource;
+    private Pantalla_Felicitacion pantalla;
 
     private void Start() {
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.clip = noteSound;
         audioSource.loop = true;
+
+        pantalla = FindAnyObjectByType<Pantalla_Felicitacion>();
     }
 
     public void OnPointerDown(PointerEventData eventData) {
@@ -20,13 +24,25 @@ public class PianoKey_Rhythm : MonoBehaviour, IPointerDownHandler, IPointerUpHan
             if (tile.note == noteName && Mathf.Abs(tile.GetComponent<RectTransform>().anchoredPosition.y) < 80f) {
                 Debug.Log("¡Perfecto!");
                 Destroy(tile.gameObject);
+                Invoke(nameof(CheckIfGameFinished), 0.1f);
                 break;
-            }
+            } 
         }
     }
 
     public void OnPointerUp(PointerEventData eventData) {
         audioSource.Stop();
     }
+    private void CheckIfGameFinished()
+    {
+        var remainingNotes = FindObjectsByType<FallingNote>(FindObjectsSortMode.None);
+        if (remainingNotes.Length == 0)
+        {
+            Debug.Log("¡Juego Terminado!");
+            StartCoroutine(pantalla.Second());
+        }
+
+    }
 }
+
 
